082cd4aec8e03abf500cc7046c9700f73cd8a9de
Dillan Haria <dillan.haria@gmail.com>
2026-03-14 02:15:48 +0000

    Inventory System basics added

--- Car Dealership.py
+++ Car Dealership.py
@@ -6,33 +6,56 @@
 
 # ---Methods---
 def show_menu():
-    print("/n=== Car Dealership Simulator ===")
+    print("\n=== Car Dealership Simulator ===")
     print("1. View Inventory")
     print("2. Buy Car")
     print("3. Sell Car")
     print("4. View Stats")
     print("5. Exit")
     
+def view_inventory(inventory):
+    print("\n=== Current Inventory ==")
+    
+    if len(inventory) == 0:
+        print("No cars in stock.")
+    else:
+        for index, car in enumerate(inventory, start=1):
+            print(f"{index}, {car['brand']} {car['model']} | Year: {car['year']} | Mileage: {car['mileage']} | Buy Price: £{car['buy_price']} | Sell Price: £{car['sell_price']}")
+    
 def main():
+    inventory = [
+        {"brand": "BMW", "model": "320d", "year": 2018, "mileage": 54000, "buy_price": 12000, "sell_price": 14500},
+        {"brand": "Audi", "model": "A1", "year": 2021, "mileage": 12304, "buy_price": 18000, "sell_price": 20150},
+        {"brand": "Ford", "model": "Focus", "year": 2015, "mileage": 35034, "buy_price": 12300, "sell_price": 14020},
+        {"brand": "Toyota", "model": "Prius", "year": 2025, "mileage": 300, "buy_price": 16000, "sell_price": 18000},
+        {"brand": "Volkswagen", "model": "Golf", "year": 2010, "mileage": 76004, "buy_price": 7500, "sell_price": 9200}
+        ]
+                  
+                  
+                  
     running = True
+    menu = True
     
     while running:
-        show_menu()
-        choice = input("Choose an option: ")
-        
-        if choice == "1":
-            print("View Inventory selected")
-        elif choice == "2":
-            print("Buy Car selected")
-        elif choice == "3":
-            print("Sell Car selected")
-        elif choice == "4":
-            print("View Stats selected")
-        elif choice == "5":
-            print("Goodbye!")
-            running = False
-        else:
-            print("Invalid choice. Please try again")
-            
+            while menu:
+                show_menu()
+                choice = input("Choose an option: ")
+                
+                if choice == "1":
+                    print("View Inventory selected")
+                    view_inventory(inventory)
+                    menu = False
+                elif choice == "2":
+                    print("Buy Car selected")
+                elif choice == "3":
+                    print("Sell Car selected")
+                elif choice == "4":
+                    print("View Stats selected")
+                elif choice == "5":
+                    print("Goodbye!")
+                    running = False
+                else:
+                    print("Invalid choice. Please try again")
+                
 # Main
 main()
